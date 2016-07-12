769a238e533809371679fa77df2d2fd3619827eb
Antonio Fernández <antonio.fernandez@bq.com>
2016-07-12 18:10:21 +0200

    allow to add domains to discard as environment variables

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-[ -z "${SMTP_SERVER}" ] && echo "SMTP_SERVER is not set" && exit 1
-[ -z "${SMTP_USERNAME}" ] && echo "SMTP_USERNAME is not set" && exit 1
-[ -z "${SMTP_PASSWORD}" ] && echo "SMTP_PASSWORD is not set" && exit 1
+[ -z "${SMTP_SERVER}" ]        && echo "SMTP_SERVER is not set"         && exit 1
+[ -z "${SMTP_USERNAME}" ]      && echo "SMTP_USERNAME is not set"       && exit 1
+[ -z "${SMTP_PASSWORD}" ]      && echo "SMTP_PASSWORD is not set"       && exit 1
+[ -z "${DOMAINS_TO_DISCARD}" ] && echo "DOMAINS_TO_DISCARD is not set"  && exit 1
 
 #Get the domain from the server host name
 DOMAIN=`echo $SERVER_HOSTNAME |awk -F. '{$1="";OFS="." ; print $0}' | sed 's/^.//'`
+
+## /etc/postfix/main.cf
 
 #Comment default mydestination, we will set it bellow
 sed -i -e '/mydestination/ s/^#*/#/' /etc/postfix/main.cf
@@ -14,13 +17,28 @@ echo "myhostname=$SERVER_HOSTNAME"  >> /etc/postfix/main.cf
 echo "mydomain=$DOMAIN"  >> /etc/postfix/main.cf
 echo 'mydestination=$myhostname'  >> /etc/postfix/main.cf
 echo 'myorigin=$mydomain'  >> /etc/postfix/main.cf
-echo "relayhost = [$SMTP_SERVER]:587" >> /etc/postfix/main.cf
+echo "transport_maps = hash:/etc/postfix/transport" >> /etc/postfix/main.cf
 echo "smtp_use_tls=yes" >> /etc/postfix/main.cf
 echo "smtp_sasl_auth_enable = yes" >> /etc/postfix/main.cf
-echo "smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd" >> /etc/postfix/main.cf
+echo "smtp_sasl_password_maps = static:$SMTP_USERNAME:$SMTP_PASSWORD" >> /etc/postfix/main.cf
 echo "smtp_sasl_security_options = noanonymous" >> /etc/postfix/main.cf
+echo "smtp_tls_security_level = may" >> /etc/postfix/main.cf
 
-echo "[$SMTP_SERVER]:587 $SMTP_USERNAME:$SMTP_PASSWORD" >> /etc/postfix/sasl_passwd
+## /etc/postfix/transport
+
+echo "localhost discard:" >> /etc/postfix/transport
+echo "localhost.localdomain discard:" >> /etc/postfix/transport
+
+for domain in $(echo $DOMAINS_TO_DISCARD | tr ":" "\n")
+do
+  echo "funkifake.com discard:" >> /etc/postfix/transport
+done
+
+
+echo "* relay:[$SMTP_SERVER]:587" >> /etc/postfix/transport
+
+
+
 postmap /etc/postfix/sasl_passwd
 
 supervisord
