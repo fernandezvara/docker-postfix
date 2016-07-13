fd1c7716eb650e255cd94c857d86b02f4cd32414
Antonio Fernández <antonio.fernandez@bq.com>
2016-07-13 10:22:57 +0200

    add the domain from the array instead of a static string

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+[ -z "${SERVER_HOSTNAME}" ]    && echo "SERVER_HOSTNAME is not set"     && exit 1
 [ -z "${SMTP_SERVER}" ]        && echo "SMTP_SERVER is not set"         && exit 1
 [ -z "${SMTP_USERNAME}" ]      && echo "SMTP_USERNAME is not set"       && exit 1
 [ -z "${SMTP_PASSWORD}" ]      && echo "SMTP_PASSWORD is not set"       && exit 1
@@ -23,6 +24,8 @@ echo "smtp_sasl_auth_enable = yes" >> /etc/postfix/main.cf
 echo "smtp_sasl_password_maps = static:$SMTP_USERNAME:$SMTP_PASSWORD" >> /etc/postfix/main.cf
 echo "smtp_sasl_security_options = noanonymous" >> /etc/postfix/main.cf
 echo "smtp_tls_security_level = may" >> /etc/postfix/main.cf
+echo "debug_peer_list = funkifake.com" >> /etc/postfix/main.cf
+echo "debug_peer_level = 2" >> /etc/postfix/main.cf
 
 ## /etc/postfix/transport
 
@@ -31,11 +34,11 @@ echo "localhost.localdomain discard:" >> /etc/postfix/transport
 
 for domain in $(echo $DOMAINS_TO_DISCARD | tr ":" "\n")
 do
-  echo "funkifake.com discard:" >> /etc/postfix/transport
+  echo "$domain discard:" >> /etc/postfix/transport
 done
 
 echo "* relay:[$SMTP_SERVER]:587" >> /etc/postfix/transport
 
-postmap /etc/postfix/sasl_passwd
+postmap /etc/postfix/transport
 
 supervisord
